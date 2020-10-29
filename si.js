4436836e26cc3a7e316ee50c1d2723ec0d7c234f
tomasvie <65181460+tomasvie@users.noreply.github.com>
2020-10-29 20:00:19 -0300

    Create si.js

---
+++ si.js
@@ -0,0 +1,13 @@
+console.log("QUE HACES ACA? RAJA DE ACA.");
+var pregunta = prompt("Ahora te voy a mostrar unas cosas y vos tenés que poner lo que te pida, entendiste (escribir sis o non)?");
+if (pregunta == "non") {
+  alert("Bue, igual, llena este formulario:");
+}
+var nombre = prompt("Nombre:");
+var edad = prompt("Edad:");
+var sexo = prompt("Sexo:");
+var comentario = prompt("Algún comentario extra?");
+document.write("Nombre: " + nombre + "<br/>");
+document.write("Edad: " + edad + "<br/>");
+document.write("Sexo: " + sexo + "<br/><br/>");
+document.write('"' + comentario + '"');
